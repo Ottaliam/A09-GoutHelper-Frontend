029471d946ff4600f2f594a52d409548779e2d44
Ottaliam <ottaliam@gmail.com>
2023-12-27 23:28:50 +0800

    趋势图

--- miniprogram/pages/health/food-history/food-history.ts
+++ miniprogram/pages/health/food-history/food-history.ts
@@ -1,6 +1,18 @@
 // pages/health/food-history/food-history.ts
 
 import { getTodayDate } from '../../../utils/util';
+const wxCharts = require('../../../utils/wxcharts.js')
+
+interface FoodRecord {
+  period: string;
+  total_purine: number;
+}
+
+interface FoodRecordSummary {
+  last_week: FoodRecord[];
+  last_month: FoodRecord[];
+  last_year: FoodRecord[];
+}
 
 Page({
 
@@ -14,6 +26,7 @@ Page({
     indicatorLeft: '0%',
     date: getTodayDate(),
     foodRecords: [],
+    foodRecordSummary: null,
   },
 
   fetchFoodRecordsForDate(dateStr: string) {
@@ -44,18 +57,84 @@ Page({
     });
   },
 
+  fetchFoodRecordSummary() {
+    const that = this;
+    const today = getTodayDate();
+
+    wx.request({
+        url: getApp().globalData.server_address + '/record/foodRecordSummary',
+        method: 'POST',
+        data: {
+            reference_date: today,
+        },
+        success(res) {
+          const data = res.data as Record<string, any>;
+
+            if (data.status === 'success') {
+              that.setData({
+                foodRecordSummary: data.summary,
+              });
+              console.error(data.summary);
+              console.error(that.data.foodRecordSummary);
+              if (that.data.foodRecordSummary) {
+                const { periods, purines } = that.extractDataForPlotting(that.data.foodRecordSummary, "last_week");
+                that.drawLineChart(purines, periods);
+              }
+            } else {
+              console.error('数据获取失败:', data.message);
+            }
+        },
+        fail(err) {
+            console.error('请求失败:', err);
+        }
+    });
+  },
+
+  extractDataForPlotting(foodRecordSummary: FoodRecordSummary, key: keyof FoodRecordSummary): { periods: string[], purines: number[] } {
+    const records = foodRecordSummary[key];
+
+    const periods = records.slice(1).map(record => record.period);
+    const purines = records.slice(1).map(record => record.total_purine);
+
+    return { periods, purines };
+  },
+
+  drawLineChart(data: number[], categories: string[]) {
+    const systemInfo = wx.getSystemInfoSync();
+    const screenWidth = systemInfo.screenWidth;
+  
+    new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: categories,
+      series: [{
+        name: '嘌呤摄入量',
+        data: data,
+        format: (val: number) => `${val.toFixed(2)}`
+      }],
+      yAxis: {
+        title: '嘌呤摄入量',
+        format: (val: number) => val.toFixed(2),
+        min: 0
+      },
+      width: screenWidth,
+      height: 300
+    });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
     this.fetchFoodRecordsForDate(getTodayDate());
+    this.fetchFoodRecordSummary();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    
   },
 
   /**
@@ -121,6 +200,27 @@ Page({
       activeTab: tab,
       indicatorLeft: this.calculateIndicatorPosition(tab)
     });
+
+    switch (tab) {
+      case 'week':
+        if (this.data.foodRecordSummary) {
+          const { periods, purines } = this.extractDataForPlotting(this.data.foodRecordSummary, "last_week");
+          this.drawLineChart(purines, periods);
+        }
+        break;
+      case 'month':
+        if (this.data.foodRecordSummary) {
+          const { periods, purines } = this.extractDataForPlotting(this.data.foodRecordSummary, "last_month");
+          this.drawLineChart(purines, periods);
+        }
+        break;
+      case 'year':
+        if (this.data.foodRecordSummary) {
+          const { periods, purines } = this.extractDataForPlotting(this.data.foodRecordSummary, "last_year");
+          this.drawLineChart(purines, periods);
+        }
+        break;
+    }
   },
 
   calculateIndicatorPosition(tab: string): string {
